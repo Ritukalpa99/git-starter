a53a77c97eec2f1bf56dd00f95fce0c6ee065213
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-03-21 17:17:02 +0530

    task 2 done

---
+++ dom.js
@@ -0,0 +1,16 @@
+// Examime the document object
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = "Sharpener"
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// console.log(document.forms);
+// console.log(document.images);
+
+// Get element by id
+console.log(document.getElementById('header-title'));

c55cf1f4db383922d710f52c315f6b5f98b690a2
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-03-22 19:16:11 +0530

    modify-dom task done

--- dom.js
+++ dom.js
@@ -7,7 +7,7 @@ var itemList = document.querySelector('#items');
 // console.log(itemList.parentNode.parentNode.parentNode);
 
 // // parentElement
-// console.log(itemList.parentElement);
+console.log(itemList.parentElement);
 // console.log(itemList.parentElement.parentElement);
 // console.log(itemList.parentElement.parentElement.parentElement);
 
@@ -36,3 +36,28 @@ var itemList = document.querySelector('#items');
 // previousSibling && previousElementSibling
 // console.log(itemList.previousSibling);
 // console.log(itemList.previousElementSibling);
+
+//
+
+// createElement
+
+// Create a div
+var newDiv = document.createElement('div');
+
+newDiv.className = 'hello';
+newDiv.id = 'hello1';   
+
+newDiv.setAttribute('title', 'hello Div');
+
+// create a textnode
+var newDivText = document.createTextNode('Hello world');
+
+// Add text to div
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container')
+var h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+
+container.insertBefore(newDiv,h1);

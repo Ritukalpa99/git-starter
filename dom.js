c69958a960100385054c8a1b0bae4c31b4e2556f
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-03-22 15:24:19 +0530

    tagName task done

--- dom.js
+++ dom.js
@@ -1,5 +1,5 @@
 // Get elements by classname
-var items = document.getElementsByClassName('list-group-item');
+// var items = document.getElementsByClassName('list-group-item');
 // console.log(items);
 // console.log(items[1]);
 // items[1].textContent = 'Hello 2';
@@ -11,8 +11,16 @@ var items = document.getElementsByClassName('list-group-item');
 // for(var  i = 0; i < items.length; i++) {
 //     items[i].style.backgroundColor = '#f4f4f4';
 // }
-items[2].style.backgroundColor = 'green';
+// items[2].style.backgroundColor = 'green';
 
-for(var i =0 ; i < items.length; i++) {
-    items[i].style.fontWeight = 'bold';
-}
+// for(var i =0 ; i < items.length; i++) {
+//     items[i].style.fontWeight = 'bold';
+// }
+
+// Get element by tagName
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+li[1].style.fontWeight = 'bold';
+li[1].style.backgroundColor = 'yellow';

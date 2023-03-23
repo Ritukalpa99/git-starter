ab377464bb1a65a347c288d78178fc07318cb5f6
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-03-23 18:42:27 +0530

    store users in right format task done

--- main.js
+++ main.js
@@ -7,7 +7,14 @@ function saveDetails(e) {
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     // console.log("submit clicked", name, email);
-    localStorage.setItem('name', name);
-    localStorage.setItem('email', email);
+    // localStorage.setItem('name', name);
+    // localStorage.setItem('email', email);
+    let data = {
+        'name' : name,
+        'email' : email
+    };
+    let data_serialized = JSON.stringify(data);
+    localStorage.setItem('myObj', data_serialized);
+    console.log(localStorage.getItem('myObj'));
     alert('Data Saved');
 }

ad8a6aea33e58b557e3d97c5e72b18cc758d1ff1
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-03-23 17:30:13 +0530

    filter task done

--- dom.js
+++ dom.js
@@ -1,54 +1,83 @@
-var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
+var form = document.getElementById("addForm");
+var itemList = document.getElementById("items");
+var filter = document.getElementById("filter");
 
 // Form submit event
-form.addEventListener('submit', addItem);
+form.addEventListener("submit", addItem);
 
 // Delete event
-itemList.addEventListener('click', removeItem);
+itemList.addEventListener("click", removeItem);
+
+// Filter event
+filter.addEventListener("keyup", filterItems);
 
 // add Item
 function addItem(e) {
-    e.preventDefault();
-    
-    // get input value
-    var newItem = document.getElementById('item').value;
+  e.preventDefault();
 
-    // create new li element
-    var li = document.createElement('li');
-    li.className = 'list-group-item';
-    // Add text node with input value
-    li.appendChild(document.createTextNode(newItem))
+  // get input value
+  var newItem = document.getElementById("item").value;
+  var newItemDesc = document.getElementById("item-desc").value;
+  // create new li element
+  var li = document.createElement("li");
+  li.className = "list-group-item";
 
-    // create edit button element
-    var editBtn = document.createElement('button');
-    editBtn.className = 'btn btn-info btn-sm float-right edit ml-2';
-    // Append text node
-    editBtn.appendChild(document.createTextNode('Edit'));
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(" " + newItemDesc));
 
-    // Append edit button to li
-    li.appendChild(editBtn);
+  // create edit button element
+  var editBtn = document.createElement("button");
+  editBtn.className = "btn btn-info btn-sm float-right edit ml-2";
+  // Append text node
+  editBtn.appendChild(document.createTextNode("Edit"));
 
-    // create del button element
-    var deleteBtn = document.createElement('button');
-    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-    // Append text node
-    deleteBtn.appendChild(document.createTextNode('X'));
+  // Append edit button to li
+  li.appendChild(editBtn);
 
-    // Append del button to li
-    li.appendChild(deleteBtn);
+  // create del button element
+  var deleteBtn = document.createElement("button");
+  deleteBtn.className = "btn btn-danger btn-sm float-right delete";
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode("X"));
 
-    // Append li to list
-    itemList.appendChild(li);
+  // Append del button to li
+  li.appendChild(deleteBtn);
 
-    form.reset();
+  // Append li to list
+  itemList.appendChild(li);
+
+  form.reset();
 }
 
-function removeItem(e){
-    if(e.target.classList.contains('delete')){
-      if(confirm('Are You Sure?')){
-        var li = e.target.parentElement;
-        itemList.removeChild(li);
-      }
+function removeItem(e) {
+  if (e.target.classList.contains("delete")) {
+    if (confirm("Are You Sure?")) {
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
     }
   }
+}
+
+// Filter Items
+function filterItems(e) {
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  
+
+  // Get list, we get an HTML collections, need to convert it into an array
+  var items = itemList.getElementsByTagName("li");
+
+  // Convert to an array
+  Array.from(items).forEach(function (item) {
+    var itemNameTitle = item.firstChild.textContent;
+    var itemNameDesc = item.childNodes[1].textContent;
+
+    // console.log(itemNameTitle ,itemNameDesc);
+    if (itemNameTitle.toLowerCase().includes(text) || itemNameDesc.toLowerCase().includes(text) ) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
